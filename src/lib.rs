9721ad63d10d2725d18288e7a5f12a0f30472659
Kurt Braun <kbraun@kbraun.dev>
2023-04-16 15:12:15 -0500

    starting work on intersections

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,6 @@ pub mod color;
 pub mod matrix;
 pub mod error;
 pub mod transformation;
+pub mod ray;
+pub mod shape;
 pub(crate) mod util;
---
+++ src/ray.rs
@@ -0,0 +1,60 @@
+use crate::{
+    error::{RayTraceError, RayTraceResult},
+    tuple::Tuple,
+};
+
+#[derive(Debug)]
+pub struct Ray {
+    origin: Tuple,
+    direction: Tuple,
+}
+
+impl Ray {
+    fn try_new(origin: Tuple, direction: Tuple) -> RayTraceResult<Self> {
+        if origin.is_point() && direction.is_vector() {
+            Ok(Self { origin, direction })
+        } else {
+            Err(RayTraceError::RayCreationError(origin, direction))
+        }
+    }
+
+    fn origin(&self) -> &Tuple {
+        &self.origin
+    }
+
+    fn direciton(&self) -> &Tuple {
+        &self.direction
+    }
+
+    fn position(&self, position: f64) -> Tuple {
+        self.origin + (self.direction * position)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creating_and_querying_a_ray() {
+        let origin = Tuple::point(1.0, 2.0, 3.0);
+        let direction = Tuple::vector(4.0, 5.0, 6.0);
+        let r = Ray::try_new(origin, direction);
+
+        assert!(r.is_ok());
+
+        let r = r.unwrap();
+
+        assert_eq!(origin, *r.origin());
+        assert_eq!(direction, *r.direciton());
+    }
+
+    #[test]
+    fn computing_a_point_from_a_distance() {
+        let r = Ray::try_new(Tuple::point(2.0, 3.0, 4.0), Tuple::vector(1.0, 0.0, 0.0)).unwrap();
+        assert_eq!(Tuple::point(2.0, 3.0, 4.0), r.position(0.0));
+        assert_eq!(Tuple::point(3.0, 3.0, 4.0), r.position(1.0));
+        assert_eq!(Tuple::point(1.0, 3.0, 4.0), r.position(-1.0));
+        assert_eq!(Tuple::point(4.5, 3.0, 4.0), r.position(2.5));
+    }
+}
