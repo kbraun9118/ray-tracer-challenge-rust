663103fb4768229af03c1ab579032ca052d6b304
Kurt Braun <kbraun@shelterinsurance.com>
2024-09-05 11:02:21 -0500

    Added cube functionality

--- src/shape/cube.rs
+++ src/shape/cube.rs
@@ -1,6 +1,11 @@
 use std::{f64::INFINITY, mem::swap};
 
-use crate::{intersection::ray::Ray, transformation::Transformation, tuple::Tuple, util};
+use crate::{
+    intersection::ray::Ray,
+    transformation::Transformation,
+    tuple::Tuple,
+    util::{self, eq_f64},
+};
 
 use super::{material::Material, Shape};
 
@@ -44,14 +49,18 @@ impl Shape for Cube {
     }
 
     fn local_intersect(&self, ray: Ray) -> Vec<f64> {
-        let (xtmin, xtmax) = dbg!(check_axis(ray.origin().x(), ray.direction().x()));
-        let (ytmin, ytmax) = dbg!(check_axis(ray.origin().y(), ray.direction().y()));
-        let (ztmin, ztmax) = dbg!(check_axis(ray.origin().z(), ray.direction().z()));
+        let (xtmin, xtmax) = check_axis(ray.origin().x(), ray.direction().x());
+        let (ytmin, ytmax) = check_axis(ray.origin().y(), ray.direction().y());
+        let (ztmin, ztmax) = check_axis(ray.origin().z(), ray.direction().z());
 
         let tmin = xtmin.max(ytmin).max(ztmin);
         let tmax = xtmax.min(ytmax).min(ztmax);
 
-        vec![tmin, tmax]
+        if tmin > tmax {
+            vec![]
+        } else {
+            vec![tmin, tmax]
+        }
     }
 
     fn transformation(&self) -> Transformation {
@@ -71,12 +80,20 @@ impl Shape for Cube {
     }
 
     fn local_normal_at(&self, point: Tuple) -> Tuple {
-        todo!()
+        let max_c = point.x().abs().max(point.y().abs()).max(point.z().abs());
+
+        if eq_f64(max_c, point.x().abs()) {
+            Tuple::vector(point.x(), 0.0, 0.0)
+        } else if eq_f64(max_c, point.y().abs()) {
+            Tuple::vector(0.0, point.y(), 0.0)
+        } else {
+            Tuple::vector(0.0, 0.0, point.z())
+        }
     }
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use crate::tuple::Tuple;
 
     use super::*;
@@ -128,14 +145,63 @@ mod test {
             ),
         ];
 
+        let c = Cube::new();
         for (origin, direction, t1, t2) in input {
-            let c = Cube::new();
             let r = Ray::new(origin, direction);
             let xs = c.local_intersect(r);
             assert_eq!(xs.len(), 2);
             assert_eq!(xs[0], t1);
             assert_eq!(xs[1], t2);
         }
+    }
 
+    #[test]
+    fn a_ray_misses_a_cube() {
+        let input = vec![
+            (
+                Tuple::point(-2.0, 0.0, 0.0),
+                Tuple::vector(0.2673, 0.5345, 0.8018),
+            ),
+            (
+                Tuple::point(0.0, -2.0, 0.0),
+                Tuple::vector(0.8018, 0.2673, 0.5345),
+            ),
+            (
+                Tuple::point(0.0, 0.0, -2.0),
+                Tuple::vector(0.5345, 0.8018, 0.2673),
+            ),
+            (Tuple::point(2.0, 0.0, 2.0), Tuple::vector(0.0, 0.0, -1.0)),
+            (Tuple::point(0.0, 2.0, 2.0), Tuple::vector(0.0, -1.0, 0.0)),
+            (Tuple::point(2.0, 2.0, 0.0), Tuple::vector(-1.0, 0.0, 0.0)),
+        ];
+
+        let c = Cube::new();
+        for (origin, direction) in input {
+            let r = Ray::new(origin, direction);
+            let xs = c.local_intersect(r);
+            assert!(dbg!(xs.is_empty()));
+        }
+    }
+
+    #[test]
+    fn the_normal_on_the_surface_of_a_cube() {
+        let input = vec![
+            (Tuple::point(1.0, 0.5, -0.8), Tuple::vector(1.0, 0.0, 0.0)),
+            (Tuple::point(-1.0, -0.2, 0.9), Tuple::vector(-1.0, 0.0, 0.0)),
+            (Tuple::point(-0.4, 1.0, -0.1), Tuple::vector(0.0, 1.0, 0.0)),
+            (Tuple::point(0.3, -1.0, -0.7), Tuple::vector(0.0, -1.0, 0.0)),
+            (Tuple::point(-0.6, 0.3, 1.0), Tuple::vector(0.0, 0.0, 1.0)),
+            (Tuple::point(0.4, 0.4, -1.0), Tuple::vector(0.0, 0.0, -1.0)),
+            (Tuple::point(1.0, 1.0, 1.0), Tuple::vector(1.0, 0.0, 0.0)),
+            (
+                Tuple::point(-1.0, -1.0, -1.0),
+                Tuple::vector(-1.0, 0.0, 0.0),
+            ),
+        ];
+        let c = Cube::new();
+        for (point, normal) in input {
+            let n = c.local_normal_at(point);
+            assert_eq!(n, normal);
+        }
     }
 }
